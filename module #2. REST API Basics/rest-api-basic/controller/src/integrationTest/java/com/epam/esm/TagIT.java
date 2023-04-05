5ffe353d65e047dc5e958f0da8f7cf983c4e787a
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-04-05 22:27:19 +0300

    looking for appcontext loading failure solution in integration tests

---
+++ module #2. REST API Basics/rest-api-basic/controller/src/integrationTest/java/com/epam/esm/TagIT.java
@@ -0,0 +1,44 @@
+package com.epam.esm;
+
+import com.epam.esm.configuration.initializer.WebAppInitializer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {WebAppInitializer.class})
+@WebAppConfiguration()
+@ActiveProfiles("test")
+class TagIT {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean("tagCController"));
+    }
+}
