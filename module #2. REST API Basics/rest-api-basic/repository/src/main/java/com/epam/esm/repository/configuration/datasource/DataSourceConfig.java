a8e8f487f6a00753b3992b70b4d621b0be297192
WSDream <beskorovajny@gmail.com>
2023-04-20 12:37:45 +0300

    Delete module #2. REST API Basics directory

--- module #2. REST API Basics/rest-api-basic/repository/src/main/java/com/epam/esm/repository/configuration/datasource/DataSourceConfig.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.repository.configuration.datasource;
-
-import javax.sql.DataSource;
-
-public interface DataSourceConfig {
-    DataSource getDataSource();
-}
--- module #2. REST API Basics/rest-api-basic/service/src/main/java/com/epam/esm/service/mapping/MappingService.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.service.mapping;
-
-public interface MappingService<T, U> {
-    T mapFromDto(U dto);
-
-    U mapToDto(T model);
-}
