ca89fdb2a63cb5eb0a55506f93294783820f38ae
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-04-23 16:57:17 +0300

    module#2 refactored to spring boot

--- module #3. REST API Advanced/rest-advanced/api/src/main/java/com/epam/esm/api/ApiApplication.java
+++ module #3. REST API Advanced/rest-advanced/api/src/main/java/com/epam/esm/api/ApiApplication.java
@@ -3,11 +3,11 @@ package com.epam.esm.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.epam.esm")
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
