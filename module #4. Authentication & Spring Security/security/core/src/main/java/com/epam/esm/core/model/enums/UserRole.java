687234abd71375182447343bc51b8a69e19e6385
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-06-18 14:59:38 +0300

    minor fix in security filter chain matchers

--- module #4. Authentication & Spring Security/security/core/src/main/java/com/epam/esm/core/model/enums/UserRole.java
+++ module #4. Authentication & Spring Security/security/core/src/main/java/com/epam/esm/core/model/enums/UserRole.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum UserRole {
-    CUSTOMER(1, "Customer"),
-    ADMIN(2, "Admin");
+    CUSTOMER(1, "CUSTOMER"),
+    ADMIN(2, "ADMIN");
 
     private final int id;
     private final String roleName;
