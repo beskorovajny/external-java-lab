2bf9a930dab3cbba28d292c3e147e1874bdd4412
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-09-06 15:58:02 +0300

    Module #7/JavaScript/part #2: first attempt to create an html structure using js

--- module #7. UI/HTML&CSS/js/part-2/part-2.js
+++ module #7. UI/HTML&CSS/js/part-2/part-2.js
@@ -69,5 +69,76 @@ restoreBtn.onclick = function() {
 
 
 /**
- *
+ * Fulfill card
  */
+function fulfillCard() {
+    // Create the outer div with class "cards-box"
+    const cardsBox = document.createElement('div');
+    cardsBox.className = 'cards-box';
+
+// Create the inner div with class "item-card"
+    const itemCard = document.createElement('div');
+    itemCard.className = 'item-card';
+
+// Create the first inner div with class "w-100 h-60 bg-grey"
+    const imageDiv = document.createElement('div');
+    imageDiv.className = 'w-100 h-60 bg-grey';
+
+// Create the div with class "it-left-section"
+    const leftSection = document.createElement('div');
+    leftSection.className = 'it-left-section';
+
+// Create the "Coupon name" paragraph
+    const couponName = document.createElement('p');
+    couponName.className = 'text-left mt-10';
+    couponName.textContent = 'Coupon name';
+
+// Create the description paragraph
+    const description = document.createElement('p');
+    description.className = 'mt-3 text-muted text-left fs-10';
+    description.textContent = 'Some brief description';
+
+// Create the price paragraph
+    const price = document.createElement('p');
+    price.className = 'mt-15 text-left';
+    price.textContent = '$235';
+
+// Create the div with class "it-right-section"
+    const rightSection = document.createElement('div');
+    rightSection.className = 'it-right-section';
+
+// Create the "favorite" link
+    const favoriteLink = document.createElement('a');
+    favoriteLink.href = '#';
+    favoriteLink.className = 'material-symbols-outlined';
+    favoriteLink.textContent = 'favorite';
+
+// Create the "Expires in N days" paragraph
+    const expiresParagraph = document.createElement('p');
+    expiresParagraph.className = 'text-muted mt-3';
+    expiresParagraph.textContent = 'Expires in N days';
+
+// Create the "Add to cart" button
+    const addToCartButton = document.createElement('button');
+    addToCartButton.className = 'mt-10';
+    addToCartButton.textContent = 'Add to cart';
+
+// Append the elements to their respective parent elements
+    leftSection.appendChild(couponName);
+    leftSection.appendChild(description);
+    leftSection.appendChild(price);
+
+    rightSection.appendChild(favoriteLink);
+    rightSection.appendChild(expiresParagraph);
+    rightSection.appendChild(addToCartButton);
+
+    itemCard.appendChild(imageDiv);
+    itemCard.appendChild(leftSection);
+    itemCard.appendChild(rightSection);
+
+    cardsBox.appendChild(itemCard);
+
+// Finally, append the "cardsBox" div to the document body or any other container you want.
+    document.body.appendChild(cardsBox);
+
+}
