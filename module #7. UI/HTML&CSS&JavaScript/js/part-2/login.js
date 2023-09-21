196992de05675bd2f4b165c35a8cc31eb29f6525
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-09-21 17:52:37 +0300

    login fixed

--- module #7. UI/HTML&CSS&JavaScript/js/part-2/login.js
+++ module #7. UI/HTML&CSS&JavaScript/js/part-2/login.js
@@ -36,14 +36,14 @@ async function login(email, password) {
 
 
 async function processLogin() {
-    const homePage = 'http://localhost:63342/external-java-lab/module%20%237.%20UI/HTML&CSS/home.html?_ijt=qlphio5aveel866866rp8d9kov&_ij_reload=RELOAD_ON_SAVE'
+    const homePage = 'http://localhost:63342/external-lab-java/module%20%237.%20UI/HTML&CSS&JavaScript/home.html?_ijt=tt9kdbuhek60pal1t5p5bnovti&_ij_reload=RELOAD_ON_SAVE'
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
     const data = await login(email, password)
     const token = data.accessToken
 
     try {
-        saveTokenToLocalStorage(token)
+        saveTokenToLocalStorage(token.toString())
         window.location.href = homePage
     } catch (error) {
         console.error('Failure in processLogin:', error.message, error.stack)
